4763910fe623371b2153b75f76e79bcb70611b56
Carlos Valarezo Loaiza <carlos.valarezo@gmail.com>
2017-12-13 14:20:55 -0500

    Getting the list of data to start processing

--- src/webcrawler/models/data.clj
+++ src/webcrawler/models/data.clj
@@ -1,8 +1,11 @@
 (ns webcrawler.models.data
   (:require [net.cgrand.enlive-html :as enlive]
-            [webcrawler.views.layout :as layout]))
+            [webcrawler.views.layout :as layout]
+            [webcrawler.utils.util :as util]))
 
 (def url "https://news.ycombinator.com/")
+
+(def _titles [{:title ""}])
 
 (defn getUrl [url]
   (enlive/html-resource (java.net.URL. url)))
@@ -19,18 +22,20 @@
 (defn getPoints []
   (map enlive/text (enlive/select (getUrl url) [:td.subtext :span.score])))
 
-(defn checkNotComments [element]
-  (.contains  element "ago"))
-
-
 (defn getComments []
-  (filter checkNotComments (map enlive/text (enlive/select (getUrl url) [:td.subtext enlive/last-child]))))
+  (filter util/checkNotComments (map enlive/text (enlive/select (getUrl url) [:td.subtext enlive/last-child]))))
 
+(def titleUnit (map (fn [x y] {:title x :rank y}) (getTitle) (getRank)))
+
+;(defn getTitlesMap []
+;  (map #(prn (str %)) (getTitle)))
+
+;(defn getTitlesMap []
+;(doseq [title (getTitle)]
+;  (prn (str title))) )
 
 
 (defn print-headlines-and-points []
-  (prn (getComments) )
+  (prn titleUnit)
   (let [lista (concat (getRank) (getTitle) (getPoints) (getSource) (getComments))]
-    (prn lista)
-    (layout/common lista)
-    ))
+    (layout/common lista)))

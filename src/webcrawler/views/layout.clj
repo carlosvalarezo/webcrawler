a070eab3227263c2e9f3c88331ce7bd6c4d90b28
carlos valarezo loaiza <carlos.valarezo@gmail.com>
2017-12-14 04:37:20 -0500

    css ready

--- src/webcrawler/views/layout.clj
+++ src/webcrawler/views/layout.clj
@@ -1,20 +1,24 @@
 (ns webcrawler.views.layout
   (:require [hiccup.page :refer [html5 include-css]]))
 
-(defn componer [lines]
-  (map (fn [line] (str "<div> " line "</div><br/>")) lines))
+(defn composeHtml [lines]
+  (map (fn [line] [:div {:class "newsUnit"}
+                   [:span {:class "rank"} (:rank line)]
+                   [:span {:class "headline"} (:title line)]
+                    [:span {:class "source"} (str "(" (:source line) ")")]
+                    [:div {:class "subtext"}  [:span {:class "points"} (str "Points :" (:points line))]
+                     [:span {:class "comments"} (str "Comments :" (:comments line))]]]) lines))
 
 (defn common [lines]
-  (let [_lines (componer lines)]
+  (let [_lines (composeHtml lines)]
     (html5
       (include-css "/css/screen.css")
+      [:h1 (str "WebCrawler demo")]
       [:body [:div {:class "leftMenu"}
-              [:div [:a { :href "/"} "Home"]]
-              [:div [:a { :href "/sortFirstCriteria"} "Sort first criteria"]]
-               [:div [:a { :href "/sortSecondCriteria"} "Sort second criteria"]]]
-               [:div {:class "container"} _lines]])
-    ))
-
+              [:div [:a {:href "/"} "Home"]]
+              [:div [:a {:href "/sortFirstCriteria"} "Sort first criteria"]]
+              [:div [:a {:href "/sortSecondCriteria"} "Sort second criteria"]]]
+       [:div {:class "container"} _lines]])))
 
 
 
